c30db9df55eed943bacc70dd7a021cb503508516
infinitepower18 <44692189+infinitepower18@users.noreply.github.com>
2025-02-08 01:28:02 +0000

    Disable tabs and don't quit when all windows are closed

--- CatalystDetect/AppDelegate.swift
+++ CatalystDetect/AppDelegate.swift
@@ -8,7 +8,7 @@
 import AppKit
 
 class AppDelegate: NSObject, NSApplicationDelegate {
-    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-        return true
+    func applicationWillFinishLaunching(_ notification: Notification) {
+        NSWindow.allowsAutomaticWindowTabbing = false
     }
 }
